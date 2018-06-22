53f39c71ca5b46d5af743555e6196d38a8896102
Ryan Dahl <ry@tinyclouds.org>
2018-06-22 23:19:43 +0200

    Call into JS from rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate libc;
 use libc::c_char;
 use libc::c_int;
+use libc::c_void;
 use std::ffi::CStr;
 use std::ffi::CString;
+use std::ptr;
+
+#[repr(C)]
+struct deno_buf {
+    data: *const c_char,
+    len: c_int, // TODO(ry) should be size_t.
+}
+
+#[repr(C)]
+struct Deno {
+    _unused: [u8; 0],
+}
+
+type DenoSubCb = extern "C" fn(d: *mut Deno, channel: *const c_char, buf: deno_buf);
 
 #[link(name = "deno", kind = "static")]
 extern "C" {
-    fn deno_v8_version() -> *const c_char;
     fn deno_init();
+    fn deno_v8_version() -> *const c_char;
     fn deno_set_flags(argc: *mut c_int, argv: *mut *mut c_char);
+
+    fn deno_new(data: *const c_void, cb: DenoSubCb) -> *const Deno;
+    fn deno_delete(d: *const Deno);
+    fn deno_last_exception(d: *const Deno) -> *const c_char;
+    fn deno_set_response(d: *const Deno, buf: deno_buf);
+    fn deno_execute(d: *const Deno, js_filename: *const c_char, js_source: *const c_char) -> c_int;
 }
 
 // Pass the command line arguments to v8.
@@ -46,15 +67,37 @@ fn set_flags() -> Vec<String> {
         .collect::<Vec<_>>()
 }
 
+extern "C" fn on_message(_d: *mut Deno, _channel: *const c_char, _buf: deno_buf) {
+    println!("got message in rust");
+}
+
+// wraps deno_execute
+fn execute(d: *const Deno, js_filename: &str, js_source: &str) -> bool {
+    let filename = CString::new(js_filename).unwrap();
+    let source = CString::new(js_source).unwrap();
+    let r = unsafe { deno_execute(d, filename.as_ptr(), source.as_ptr()) };
+    r != 0
+}
+
 fn main() {
-    println!("Hi");
     let args = set_flags();
     unsafe { deno_init() };
+
+    /*
     let v = unsafe { deno_v8_version() };
     let c_str = unsafe { CStr::from_ptr(v) };
     let version = c_str.to_str().unwrap();
     println!("version: {}", version);
-    for arg in args {
-        println!("arg: {}", arg);
+    */
+
+    let d = unsafe { deno_new(ptr::null(), on_message) };
+
+    let ok = execute(d, "deno_main.js", "denoMain();");
+    if !ok {
+        let err = unsafe { deno_last_exception(d) };
+        println!("Error!\n");
+        std::process::exit(1);
     }
+
+    unsafe { deno_delete(d) }
 }
